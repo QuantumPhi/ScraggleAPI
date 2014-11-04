c49c63f7e839c35839d2d95eda7b785a27eeedd5
Tarik Onalan <phi.quantum@gmail.com>
2014-11-03 22:24:00 -0800

    Apparently I changed something

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express    = require("express"),
     bodyparser = require("body-parser"),
-    app        = express()
+    app        = express(),
+    port       = process.env.PORT || 8080
 
-app.use(bodyparser.urlencoded({ encoded: true}))
-app.use(bodyparser())
+app.use(bodyparser.urlencoded({ extended: true}))
+app.use(bodyparser.json())
 app.use(function(req, res, next) {
     console.log("Time: %d -> %s", Date.now(), req.path)
     next()
 })
+
+app.get("/board", function(req, res) {
+    var letters = "abcdefghijklmnopqrstuvwxyz"
+    var text = ""
+
+    for(var i = 0; i < 16; i++)
+        text += letters.charAt(Math.floor(Math.random() * letters.length))
+
+    res.json({ "letters": text })
+})
+
+app.listen(port)
+console.log("API is located on port: %d", port)
